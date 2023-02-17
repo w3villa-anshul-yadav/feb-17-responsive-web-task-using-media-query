2fafba01d916706cb61a8b7918b5fefd250f29b5
Anshul Yadav <anshul.yadav@w3villa.com>
2023-02-17 16:45:56 +0530

    style changes

---
+++ assect/script/script.js
@@ -0,0 +1,9 @@
+
+// console.log("hello");
+let navBarToggler=document.getElementById("navbar-toggler");
+
+navBarToggler.addEventListener("click",function(){
+let navBar=document.getElementById("navbar");
+navBar.classList.toggle("toggle-navbar");
+})
+
